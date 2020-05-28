80c139ef893f1d37a43b533a312f89c2be50a5ac
esyywar <rahuleswar@gmail.com>
2020-05-28 01:56:07 -0400

    projTag bug fixed

--- src/components/subComponents/ProjTag.js
+++ src/components/subComponents/ProjTag.js
@@ -1,12 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 
 // Project tag items rendered in Projects.js
 function ProjTag(props) {
-    const tagStyle = (props.active) ? "tag-active" : "tag-inactive"
+    const [activeTag, setActiveTag] = useState(props.active)
+
+    function handleClick() {
+        setActiveTag(!activeTag)
+        props.onClick(props.tag)
+    }
 
     return (
-        <div className={`tag-item ${tagStyle}`} onClick={() => props.onClick(props.tag)}>
+        <div className={`tag-item ${activeTag ? "tag-active" : "tag-inactive"}`} onClick={handleClick}>
             <p>{props.tag}</p>
         </div>            
     )
